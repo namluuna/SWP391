ed01a4b3420e2afec7896d89e16a428e9e06d3a3
ng2khang <khangnhhe160625@fpt.edu.vn>
2024-01-10 11:53:02 +0700

    create province DAO and model

--- SWP_391/src/java/DAO/DBContext.java
+++ SWP_391/src/java/DAO/DBContext.java
@@ -4,10 +4,37 @@
  */
 package DAO;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author ifyou
  */
 public class DBContext {
-    
+    protected static Connection connection;
+
+    public DBContext() {
+        try {
+            String user = "sa";
+            String pass = "sa";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=OSS";
+
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+
+        } catch (Exception ex) {
+            System.err.println("Cannot connect database, " + ex);
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        DBContext d = new DBContext();
+        System.out.println(d.connection);
+
+    }
 }
